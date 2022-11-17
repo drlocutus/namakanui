902025c6e4f0f04c0259d38c46ff85dbbcb51320
Locutus C.-W. Huang <cwhuang@asiaa.sinica.edu.tw>
2022-11-17 09:17:54 +0000

    Some updated info regarding to the power meter used in the photonics anttenuation tests.

--- src/namakanui/pmeter.py
+++ src/namakanui/pmeter.py
@@ -96,6 +96,7 @@ class PMeter(object):
             self.s.connect((self.ip, self.port))
             self.s.send(b'*idn?\n')
             idn = self.s.recv(256)
+            ## Because N1914A was used in photonics tuning:
             #if b'N1913A' not in idn:
             #    raise RuntimeError(f'expected power meter model N1913A, but got {idn}')
             # TODO control setup via config
